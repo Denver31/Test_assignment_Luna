0e8cb7efee49bddb4eb5c45e6c03970d7c1218c2
Denver31 <daniil.igoshev05@gmail.com>
2026-02-03 19:16:03 -0300

    подключение роутеров

--- app/main.py
+++ app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 
 from app.activities.router import router as activities_router
 from app.buildings.router import router as buildings_router
+from app.organizations.router import router as organizations_router
+from app.organizations.phones.router import router as phones_router
 
 app = FastAPI()
 
 app.include_router(activities_router)
 app.include_router(buildings_router)
+app.include_router(phones_router)
+app.include_router(organizations_router)
